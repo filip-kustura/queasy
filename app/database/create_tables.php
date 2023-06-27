31924407d6cd723d64a123f5f0bd858c21b4fca4
fikustura <filipkustura7@gmail.com>
2023-06-27 23:16:41 +0200

    Upgrade create_tables.php & seed_tables.php
    Enable automatic table creating/inserting to if needed

--- app/database/create_tables.php
+++ app/database/create_tables.php
@@ -3,12 +3,32 @@
 // Stvaramo tablice u bazi (ako već ne postoje od ranije).
 require_once __DIR__ . '/db.class.php';
 
-create_table_users();
+$db = DB::getConnection();
+
+try {
+	$st = $db->prepare('SHOW TABLES');
+	$st->execute();
+	$tables = $st->fetchAll(PDO::FETCH_COLUMN);
+
+	$has_users_table = in_array('users', $tables);
+	$has_questions_table = in_array('questions', $tables);
+} catch( PDOException $e ) {
+	exit( "PDO error [show tables]: " . $e->getMessage() );
+}
+
+if (!$has_users_table)
+	create_table_users();
+
+if (!$has_questions_table)
+	create_table_questions();
+
+if ($has_users_table && $has_questions_table)
+	echo 'Sve potrebne tablice već postoje u bazi.';
 
 // ------------------------------------------
 function create_table_users()
 {
-	$db = DB::getConnection();
+	global $db;
 
 	// Stvaramo tablicu users.
 	// Svaki user ima svoj id (automatski će se povećati za svakog novoubačenog korisnika), username i password hash.
@@ -27,6 +47,37 @@ function create_table_users()
 	catch( PDOException $e ) { exit( "PDO error (create_table_users): " . $e->getMessage() ); }
 
 	echo "Napravio tablicu users.<br />";
+}
+
+function create_table_questions() {
+	global $db;
+
+	// Stvaramo tablicu questions.
+	/*
+	Svaki question ima:
+		- id (automatski će se povećati za svakog novoubačenog korisnika)
+		- kategoriju
+		- tekst pitanja
+		- točan odgovor
+		- tri ponuđena netočna odgovora (null ako se radi o pitanju gdje treba upisati odgovor)
+	*/
+
+	try {
+		$st = $db->prepare(
+			'CREATE TABLE IF NOT EXISTS questions (' .
+			'id int NOT NULL PRIMARY KEY AUTO_INCREMENT,' .
+			'category varchar(20) NOT NULL,' .
+			'question varchar(255) NOT NULL,' .
+			'answer varchar(50) NOT NULL,' .
+			'optionA varchar(50),' .
+			'optionB varchar(50),' .
+			'optionC varchar(50));'
+		);
+
+		$st->execute();
+	} catch( PDOException $e ) { exit( "PDO error (create_table_questions): " . $e->getMessage() ); }
+
+	echo "Napravio tablicu questions.<br>";
 }
 
 ?>
--- app/database/seed_tables.php
+++ app/database/seed_tables.php
@@ -3,12 +3,34 @@
 // Popunjavamo tablice u bazi "probnim" podacima.
 require_once __DIR__ . '/db.class.php';
 
-seed_table_users();
+$db = DB::getConnection();
+
+$users_table_empty = isTableEmpty('users');
+$questions_table_empty = isTableEmpty('questions');
+
+if ($users_table_empty)
+	seed_table_users();
+
+if ($questions_table_empty)
+	seed_table_questions();
+
+if (!$users_table_empty && !$questions_table_empty)
+	echo 'Sve potrebne tablice već su popunjene podacima.';
 
 // ------------------------------------------
+function isTableEmpty($table_name) {
+	global $db;
+	
+	$st = $db->prepare('SELECT COUNT(*) FROM ' . $table_name);
+	$st->execute();
+	$rowCount = $st->fetchColumn();
+
+	return ($rowCount === 0);
+}
+
 function seed_table_users()
 {
-	$db = DB::getConnection();
+	global $db;
 
 	// Ubaci neke korisnike u tablicu users.
 	// Uočimo da ne treba specificirati id koji se automatski poveća kod svakog ubacivanja.
@@ -25,6 +47,187 @@ function seed_table_users()
 	catch( PDOException $e ) { exit( "PDO error (seed_table_users): " . $e->getMessage() ); }
 
 	echo "Ubacio korisnike u tablicu users.<br />";
+}
+
+function seed_table_questions() {
+	global $db;
+	
+	// Ubaci neka pitanja u tablicu questions.
+	// Uočimo da ne treba specificirati id koji se automatski poveća kod svakog ubacivanja.
+	
+	// Ubacimo pitanja s ponuđenim odgovorima
+	try {
+		$st = $db->prepare( 'INSERT INTO questions(category, question, answer, optionA, optionB, optionC) VALUES (:category, :question, :answer, :optionA, :optionB, :optionC)' );
+
+		$st->execute(
+			array(
+				'category' => 'history',
+				'question' => 'What nationality was Benito Mussolini?',
+				'answer' => 'Italian',
+				'optionA' => 'Spanish',
+				'optionB' => 'French',
+				'optionC' => 'American'
+			)
+		);
+
+		$st->execute(
+			array(
+				'category' => 'geography',
+				'question' => 'What country is Biscay in?',
+				'answer' => 'Spain',
+				'optionA' => 'Japan',
+				'optionB' => 'Brazil',
+				'optionC' => 'Portugal'
+			)
+		);
+		
+		$st->execute(
+			array(
+				'category' => 'sports',
+				'question' => 'Which of these teams uses a shade of blue as one of its main colors?',
+				'answer' => 'Indianapolis Colts',
+				'optionA' => 'Cleveland Browns',
+				'optionB' => 'Cincinnati Bengals',
+				'optionC' => 'Pittsburgh Steelers'
+			)
+		);
+
+		$st->execute(
+			array(
+				'category' => 'art',
+				'question' => 'Who penned the literary classic "The Compleat Angler"?',
+				'answer' => 'Izaak Walton',
+				'optionA' => 'Lewis Carrol',
+				'optionB' => 'John Steinbeck',
+				'optionC' => 'Pearl S. Buck'
+			)
+		);
+
+		$st->execute(
+			array(
+				'category' => 'science',
+				'question' => 'What kind of wave is light?',
+				'answer' => 'Transverse and Electromagnetic',
+				'optionA' => 'Transverse and Mechanical',
+				'optionB' => 'Longitudinal and Electromagnetic',
+				'optionC' => 'Longitudinal and Mechanical'
+			)
+		);
+
+		$st->execute(
+			array(
+				'category' => 'entertainment',
+				'question' => 'What is Gru\'s goal at the beginning of Despicable me?',
+				'answer' => 'Steal the moon',
+				'optionA' => 'Steal the Egyptian pyramids',
+				'optionB' => 'Find Neptune\'s crown and save Mr. Krabs',
+				'optionC' => 'Teach himself how to fly'
+			)
+		);
+	} catch( PDOException $e ) { exit( "PDO error (seed_table_questions): " . $e->getMessage() ); }
+
+	// Ubacimo pitanja gdje treba upisati odgovor
+	try {
+		$st = $db->prepare( 'INSERT INTO questions(category, question, answer) VALUES (:category, :question, :answer)' );
+
+		$st->execute(
+			array(
+				'category' => 'history',
+				'question' => 'In which century did the Western Roman Empire fall? (1 digit)',
+				'answer' => '5'
+			)
+		);
+
+		$st->execute(
+			array(
+				'category' => 'geography',
+				'question' => 'What is the capital of Brasil?',
+				'answer' => 'brasilia'
+			)
+		);
+
+		$st->execute(
+			array(
+				'category' => 'sports',
+				'question' => 'In what sport should you "float like a butterfly and sting like a bee"?',
+				'answer' => 'boxing'
+			)
+		);
+
+		$st->execute(
+			array(
+				'category' => 'art',
+				'question' => 'Who is the Roman god of freshwater and the sea in Roman religion?',
+				'answer' => 'neptune'
+			)
+		);
+
+		$st->execute(
+			array(
+				'category' => 'science',
+				'question' => 'Approximately how long (in months) does the gestation period for cows last? (1 digit)',
+				'answer' => '9'
+			)
+		);
+
+		$st->execute(
+			array(
+				'category' => 'entertainment',
+				'question' => 'What species is SpongeBob SquarePants\' employer? (1 word, 4 letters)',
+				'answer' => 'crab'
+			)
+		);
+
+		/*$st->execute(
+			array(
+				'category' => 'history',
+				'question' => 'In which century did the Western Roman Empire fall? (1 digit)',
+				'answer' => '5'
+			)
+		);
+
+		$st->execute(
+			array(
+				'category' => 'geography',
+				'question' => 'What other island forms a nation with Trinidad?',
+				'answer' => 'tobago'
+			)
+		);
+
+		$st->execute(
+			array(
+				'category' => 'sports',
+				'question' => 'In what sport should you "float like a butterfly and sting like a bee"?',
+				'answer' => 'boxing'
+			)
+		);
+
+		$st->execute(
+			array(
+				'category' => 'art',
+				'question' => 'Who is the Roman god of freshwater and the sea in Roman religion?',
+				'answer' => 'neptune'
+			)
+		);
+
+		$st->execute(
+			array(
+				'category' => 'science',
+				'question' => 'Approximately how long (in months) does the gestation period for cows last? (1 digit)',
+				'answer' => '9'
+			)
+		);
+
+		$st->execute(
+			array(
+				'category' => 'entertainment',
+				'question' => 'What species is SpongeBob SquarePants\' employer? (1 word, 4 letters)',
+				'answer' => 'crab'
+			)
+		);*/
+	} catch( PDOException $e ) { exit( "PDO error (seed_table_questions): " . $e->getMessage() ); }
+
+	echo "Ubacio pitanja u tablicu questions.<br>";
 }
 
 ?>
