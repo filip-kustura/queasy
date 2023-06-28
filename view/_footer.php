be991620f15589e842e512b25eb5ff85149ed5dd
VilimK <vilim12k@gmail.com>
2023-06-28 22:51:00 +0200

    Footer upgraded

--- view/_footer.php
+++ view/_footer.php
@@ -1,3 +1,41 @@
+<footer class="footer">
+            <div class="quote-container">
+                <span id="quote-text">"Quizzes are not about being right or wrong; they're about learning and growth."</span>
+            </div>
+            <button class="logout-button">Log Out</button>
+        </footer>
+
+    
+    <script>
+        var quotes = [
+            "The only way to learn is to test yourself.",
+            "The quiz is the tool that makes the lesson stick.",
+            "A quiz is not only a tool for assessment; it's an opportunity for learning.",
+            "Quizzes are like a mental workout, keeping your mind sharp and agile.",
+            "Quizzes provide an opportunity to challenge yourself and push beyond your comfort zone.",
+            "A well-designed quiz can reveal the depths of your knowledge and inspire further learning.",
+            "Quizzes are not about being right or wrong; they're about learning and growth.",
+            "The beauty of a quiz lies in the discovery of what you know and what you can still learn.",
+            "Quizzes are like puzzle pieces that form a bigger picture of knowledge.",
+            "A quiz is a journey of exploration, unlocking the doors to new understanding."
+        ]; 
+
+        let currentQuoteIndex = 0;
+        const quoteTextElement = document.getElementById("quote-text");
+
+        function changeQuote() {
+            quoteTextElement.style.opacity = 0; 
+
+            setTimeout(() => {
+                currentQuoteIndex = (currentQuoteIndex + 1) % quotes.length; 
+
+                quoteTextElement.textContent = quotes[currentQuoteIndex];
+                quoteTextElement.style.opacity = 1; 
+            }, 1000); 
+        }
+
+        quoteTextElement.textContent = quotes[currentQuoteIndex];
+        setInterval(changeQuote, 3000);
+    </script>
 </body>
 </html> 
- 
--- view/_header.php
+++ view/_header.php
@@ -5,6 +5,53 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Queasy</title>
     <style>
+
+.footer {
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            background-color: #1f086c;
+            padding: 16px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            color: #da630e;
+            font-family: Arial, sans-serif;
+        }
+
+        .quote-container {
+            flex-grow: 1;
+            font-size: 14px;
+            font-weight: bold;
+            font-family: "Arial", sans-serif;
+            text-align: center;
+            animation: quote-animation 30s infinite;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .logout-button {
+            background-color: red;
+            color: #fff;
+            padding: 8px 16px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        @keyframes quote-animation {
+            0%, 20% {
+                opacity: 1;
+            }
+            25%, 95% {
+                opacity: 0;
+            }
+            100% {
+                opacity: 1;
+            }
+        }
         .footer-container {
             display: flex;
             align-items: center;
